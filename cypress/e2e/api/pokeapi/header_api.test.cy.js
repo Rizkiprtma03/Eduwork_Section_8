8659a0a2342a85db79b11716d6c334e140cf8c67
RizkiP <rizkiprtma03@gmail.com>
2023-02-27 13:18:26 +0700

    New added file 27-02-2023

--- cypress/e2e/api/pokeapi/header_api.test.cy.js
+++ cypress/e2e/api/pokeapi/header_api.test.cy.js
@@ -16,4 +16,35 @@ describe('Validate Header', () => {
       expect(response.body).property('abilities');
     });
   });
+  it('Successfully validate status code', () => {
+    cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('ditto'), cy.get('@ditto').its('status').should('eq', 200);
+  });
+  it('Successfully validate status code with params', () => {
+    cy.request({
+      methode: 'GET',
+      url: 'https://pokeapi.co/api/v2/pokemon/ditto?page=2&per_pages=1&delay=3',
+    }).as('users');
+    cy.get('@users').its('status').should('equal', 200);
+    cy.get('@users').then((response) => {
+      cy.log(JSON.stringify(response));
+    });
+  });
+  it('Successfully validate content', () => {
+    cy
+      .request({
+        method: 'GET',
+        url: 'https://pokeapi.co/api/v2/pokemon/bulbasaur',
+      })
+      .as('bulbasaur'),
+      cy.get('@bulbasaur').its('body').should('include', { name: 'bulbasaur' });
+  });
+  it('Successfully validate content body', () => {
+    var pokemon = {
+      name: 'limber',
+    };
+    cy.request('https://pokeapi.co/api/v2/pokemon/ditto?page=2&per_pages=1&delay=3', pokemon).as('limber'),
+      cy.get('@limber').then((response) => {
+        expect(response.body.name).to.eq('ditto');
+      });
+  });
 });
